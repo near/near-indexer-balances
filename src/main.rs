b01ad501a06526e00869487ee13764173a828846
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2022-05-13 11:40:50 +0400

    wip: wow it compiles

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ async fn main() -> anyhow::Result<()> {
         s3_bucket_name: opts.s3_bucket_name.clone(),
         s3_region_name: opts.s3_region_name.clone(),
         start_block_height,
-        s3_config: None
+        s3_endpoint: None,
     };
     init_tracing();
 
